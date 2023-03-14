d08b6eed863096d0a6a5930fb2b9711f20ba9dbf
eddyshrea <eddyshrea@gmail.com>
2023-03-14 18:55:28 -0500

    first commit

---
+++ src/intro/Myintro.java
@@ -0,0 +1,12 @@
+package intro;
+
+public class Myintro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My name is Ashiq");
+		System.out.println("I like programming");
+		System.out.println("I am from chicago");
+	}
+
+}
